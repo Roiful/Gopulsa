d82ede3461cd66204efcec9d62d74b75a14a6153
Roiful <Muhammadroifulanam2001@gmail.com>
2022-06-29 19:25:12 +0700

    add main

---
+++ App/Controller/Main.go
@@ -0,0 +1,7 @@
+package Main
+
+import App "https://github.com/Roiful/Gopulsa/App"
+
+func main() {
+	App.Run()
+}
---
+++ App/Controller/Server.go
@@ -0,0 +1,34 @@
+package App
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initialize() {
+	fmt.Println("Selamat Datang di PulsaGO ")
+
+	server.Router = mux.newRouter()
+
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Printf("Listening to port %s", addr)
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
+
+func Run() {
+	var server = Server{}
+
+	server.Initialize()
+	server.Run(":9000")
+}
